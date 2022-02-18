8abbd0148e288714136e53b1f9fb500708720956
WalterWj <277372664@qq.com>
2022-02-18 23:10:18 +0800

    master

--- fight.py
+++ fight.py
@@ -6,29 +6,29 @@ from sqlite3 import connect
 import time
 import random
 import os
+from tkinter.messagebox import NO
 import pyautogui
 
-time.sleep(10)
+time.sleep(3)
 
 print("开始游戏")
 start_time = time.time()
 
 while True:
-    find_img_path = os.path.join("img", "find.png")
-    connect_img_path = os.path.join("img", "connect.png")
+    shop_osPath = os.path.join("img","fight")
+    files = os.listdir(shop_osPath)
+    for file_name in files:
+        if not os.path.isdir(file_name):
+            imgs = os.path.join(shop_osPath, file_name)
+            locate = pyautogui.locateOnScreen(imgs)
+            if locate is not None:
+                point = pyautogui.center(locate)
+                pyautogui.click(point)
+                print("file: {}".format(file_name))
 
-    find_locate = pyautogui.locateOnScreen(find_img_path)
-    connect_locate = pyautogui.locateOnScreen(connect_img_path)
+    _time = "已经刷了 {} s".format(time.time()-start_time)
+    print(_time, end='')
+    print('\b' * len(_time) * 2, end='', flush=True)
 
-    if find_locate is not None:
-        find_point = pyautogui.center(find_locate)
-        pyautogui.click(find_point)
-    elif connect_locate is not None:
-        connect_point = pyautogui.center(connect_locate)
-        pyautogui.click(connect_point)
-    else:
-        _time = "已经刷了 {} s".format(time.time()-start_time)
-        print(_time, end='')
-        print('\b' * len(_time) * 2, end='', flush=True)
-
-    time.sleep(random.randint(7, 15))
+    # time.sleep(random.randint(7, 15))
+    time.sleep(random.randint(3, 6))
